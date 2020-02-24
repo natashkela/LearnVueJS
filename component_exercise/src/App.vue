4f1f650b481554d95c3c44845d7e8c17c95fa15a
nvacheishvili <nvacheishvili@wpi.edu>
2020-02-24 15:05:27 -0500

    exercise completed

--- component_exercise/src/App.vue
+++ component_exercise/src/App.vue
@@ -1,35 +1,13 @@
 <template>
     <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <header>
-                    <h1>Server Status</h1>
-                </header>
-            </div>
-        </div>
+        <header-server></header-server>
         <hr>
         <div class="row">
-            <div class="col-xs-12 col-sm-6">
-                <ul class="list-group">
-                    <li
-                            class="list-group-item"
-                            v-for="index in 5">
-                        Server #{{ index }}
-                    </li>
-                </ul>
-            </div>
-            <div class="col-xs-12 col-sm-6">
-                <p>Server Details are currently not updated</p>
-            </div>
+            <server-container></server-container>
+            <server-detail></server-detail> 
         </div>
         <hr>
-        <div class="row">
-            <div class="col-xs-12">
-                <footer>
-                    <p>All Servers are managed here</p>
-                </footer>
-            </div>
-        </div>
+        <footer-server></footer-server>
     </div>
 </template>
 
--- component_exercise/src/main.js
+++ component_exercise/src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Header from './Header.vue';
+import Footer from './Footer.vue';
+import ServerDetail from './ServerDetail.vue';
+import ServerContainer from './ServerContainer.vue';
 
+Vue.component('header-server',Header);
+Vue.component('server-container',ServerContainer);
+Vue.component('server-detail',ServerDetail);
+Vue.component('footer-server',Footer);
 new Vue({
   el: '#app',
   render: h => h(App)
