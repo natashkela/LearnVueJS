280e3ab7c68da0f1a77ab4f1dc84e3aace781439
nvacheishvili <nvacheishvili@wpi.edu>
2020-02-25 10:06:25 -0500

    named slots

--- advanced_components/src/App.vue
+++ advanced_components/src/App.vue
@@ -2,16 +2,26 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-               
+               <app-quote>
+                   <h1 slot="title">{{quoteTitle}}</h1>
+                   <p slot="content">Do you understand the meaning of it? </p>
+               </app-quote>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-
+    import Quote from './components/Quote.vue';
     export default {
-        
+    	data:function(){
+    		return {
+    			quoteTitle: 'The quote'
+            }
+        },
+    	components: {
+			'app-quote': Quote
+		}
     }
 </script>
 
